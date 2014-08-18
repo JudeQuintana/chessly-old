aa8105713829cd765e4442c691b425da49ab32da
Jude Quintana <jude.quintana@gmail.com>
2014-08-18 17:27:19 -0600

    user can login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_authenticated_user
+
 
   def new
     @user = User.new
@@ -7,12 +9,18 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:user][:email])
 
-    if @user && @user.authenticate(password: params[:user][:password])
-      # session[:]
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      @user = User.new
+      @user.errors[:base] = "Email/Password is invalid!"
+      render :new
     end
   end
 
   def destroy
-
+    session.destroy
+    redirect_to signin_path
   end
 end
---
+++ spec/features/app_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+require 'capybara/rails'
+
+feature 'Auth' do
+  scenario 'user can '
+end
