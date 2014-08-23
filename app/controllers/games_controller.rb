6dea01834bf409bd86cc13879e4d74f4595e575a
Jude Quintana <jude.quintana@gmail.com>
2014-08-23 16:25:38 -0600

    user can delete a game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,9 +12,9 @@ class GamesController < ApplicationController
     @user = User.find(session[:user_id])
 
     pgn.gsub!(/\u201c/, "\"")
-    pgn.gsub!(/\u201d/, "\"")
-    pgn.gsub!(/\r/, "")
-    pgn.gsub!(/\n/, " ")
+    pgn.gsub!(/\u201d/, "\"")#top 2 remove unicode quotation marks
+    pgn.gsub!(/\r/, "")#remove carriage return
+    pgn.gsub!(/\n/, " ")#remove new line
     # pgn.gsub!(/{.*?}/, "") #removes annotation comments
     pgn.gsub!(/\$\d+\s/, "") #removes NAG
     pgn.gsub!(/0-0/, "O-O")
@@ -30,7 +30,7 @@ class GamesController < ApplicationController
       return
     end
 
-    if game == [] || game == nil
+    if game.nil? || game.empty?
       @game = Game.new
       @game.errors[:pgn] = "text could not be parsed! Please check your formatting."
 
@@ -46,26 +46,23 @@ class GamesController < ApplicationController
     @game = Game.new(user_id: session[:user_id], game_list_id: game_list_id, event: game.tags["Event"], site: game.tags["Site"], date: game.tags["Date"], round: game.tags["Round"], white: game.tags["White"], black: game.tags["Black"], result: game.tags["Result"], pgn: pgn)
 
     if @game.save
-
-      redirect_to '/'
-
+      redirect_to root_path
     else
-
       render :new
-
     end
-
 
   end
 
 
   def show
     @user = User.find(session[:user_id])
-
-
-    # p params
     @game = Game.find(params[:id])
+  end
 
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to root_path
   end
 
 end
