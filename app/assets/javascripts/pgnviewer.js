54d9246270ff15498d14074b18a37f8bd95c040f
Jude Quintana <jude.quintana@gmail.com>
2014-08-24 22:56:21 -0600

    user can delete game along with assoc note

--- app/assets/javascripts/pgnviewer.js
+++ app/assets/javascripts/pgnviewer.js
@@ -1,5 +1,25 @@
-function buildViewer(){
+function buildViewer(pgn){
 
-
+  new PgnViewer(
+    { boardName: "demo",
+      pgnString: pgn,
+      pieceSet: 'merida',
+      pieceSize: 65,
+      highlightFromTo: true,
+//      showCoordinates: true,
+      pauseBetweenMoves: 2000,
+      moveAnimationLength: .3
+//        movesFormat: "main_on_own_line"
+//        autoScrollMoves: true,
+      //showBracketsOnVariation: false,
+//        hideBracketsOnTopLevelVariation: true,
+//        canPasteFen: true,
+//        previewMode: true,
+//        soundEnabled: false
+//        puzzle: true
+//        showNPS: true
+//        newlineForEachMainMove: false
+    }
+  );
 
 }
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
   # belongs_to :user
-  has_one :note
+  has_one :note, dependent: :destroy
 
   validates :white, :black, :result, :pgn, presence: true
 
