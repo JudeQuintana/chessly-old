a6fa190eca68d553b5184b952f67e63f16245bbe
Jude Quintana <jude.quintana@gmail.com>
2014-09-02 13:49:57 -0600

    centered chess board, wider column for game moves, relocated fen finder

--- app/controllers/fens_controller.rb
+++ app/controllers/fens_controller.rb
@@ -7,19 +7,19 @@ class FensController < ApplicationController
     game = Game.find(params[:game_id])
 
     # if params[:fen] && params[:fen] != "" #&& params[:fen] =~ /\d+/
-      hsh_i = 1
-      build_fen = PGN.parse("[Dummy \"Tag\"]" + game["pgn"]).first
-      build_fen.positions.delete_at(0)
-      build_fen.positions.each_with_index do |pos, index|
+    hsh_i = 1
+    build_fen = PGN.parse("[Dummy \"Tag\"]" + game[:pgn]).first
+    build_fen.positions.delete_at(0)
+    build_fen.positions.each_with_index do |pos, index|
 
-        if index % 2 == 0 #even
-          @fen_hsh[hsh_i] = [pos.to_fen.to_s]
-        elsif index %2 == 1
-          @fen_hsh[hsh_i].push(pos.to_fen.to_s)
-          hsh_i +=1
-        end
-
+      if index % 2 == 0 #even
+        @fen_hsh[hsh_i] = [pos.to_fen.to_s]
+      elsif index %2 == 1
+        @fen_hsh[hsh_i].push(pos.to_fen.to_s)
+        hsh_i +=1
       end
+
+    end
 
     # end
 
