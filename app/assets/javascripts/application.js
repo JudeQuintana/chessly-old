c01987b53fc12d55bbc659a00358f379cd21c671
Jude Quintana <jude.quintana@gmail.com>
2014-09-03 16:29:01 -0600

    added animation to each page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 ///
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+//= require jquery-ui/effect-clip
 // require chessboardjs/chessboard-0.3.0
 // require chessjs/chess
 //= require chesstempojs/pgnyui
